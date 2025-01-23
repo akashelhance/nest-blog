53e39f88462bbbfab62f2cb6e8e73ed7b7d7e48a
akash <akashelhance@gmail.com>
2025-01-23 22:01:07 +0530

    user service in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
+// auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module'; 
+import { JwtModule } from '@nestjs/jwt'; 
 
 @Module({
+  imports: [
+    UsersModule, // Add UsersModule to the imports array
+    JwtModule.register({
+      secret: 'yourSecretKey', // Replace with your actual secret key
+      signOptions: { expiresIn: '1h' }, // Customize expiration as needed
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: any) {
+    const payload = { id: user.id, email: user.email };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
